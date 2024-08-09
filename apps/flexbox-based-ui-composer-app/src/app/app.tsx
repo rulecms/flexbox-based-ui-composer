10c80bbf5ae39998fed5411d81c1457d61da69b4
Vishal Kumar <connectwithvkumar@gmail.com>
2024-08-09 06:52:03 -0400

    Initial commit
    To connect your workspace to Nx Cloud, push your repository
    to your git hosting provider and go to the following URL:
    
    https://cloud.nx.app/connect/le2WGEnHjF

---
+++ apps/flexbox-based-ui-composer-app/src/app/app.tsx
@@ -0,0 +1,14 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import styles from './app.module.css';
+
+import NxWelcome from './nx-welcome';
+
+export function App() {
+  return (
+    <div>
+      <NxWelcome title="flexbox-based-ui-composer-app" />
+    </div>
+  );
+}
+
+export default App;
