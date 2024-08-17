00870dbfbdb27cfe6dfd5dc04756001108ffcabd
Vishal Kumar <connectwithvkumar@gmail.com>
2024-08-17 13:33:20 -0400

    refactor

---
+++ flexbox-based-ui-composer/src/lib/ui-composer-sections/RenderSelectionPanel.tsx
@@ -0,0 +1,35 @@
+import { DraggableWithoutButtonLook } from "../DraggableWithoutButtonLook";
+import { SelectionPanel } from "./SelectionPanel";
+import { useSelector } from 'react-redux';
+
+export function RenderSelectionPanel({
+  cardGroupTitle,
+  cardTitle,
+  entries,
+}: {
+  cardGroupTitle: string;
+  cardTitle: string;
+  entries: { id: string; card: JSX.Element }[];
+}) {
+  const displayStatus = useSelector(
+    (state: { composePlayground }) =>
+      state.composePlayground.selectionCardDisplayStatuses
+  );
+
+  if (!displayStatus[cardGroupTitle]) {
+    return null;
+  }
+  return (
+    <SelectionPanel title={cardTitle}>
+      {entries.map((entry) => (
+        <div key={`${entry.id}-container`}>
+          <DraggableWithoutButtonLook
+            id={entry.id}
+          >
+            {entry.card}
+          </DraggableWithoutButtonLook>
+        </div>
+      ))}
+    </SelectionPanel>
+  );
+}
--- flexbox-based-ui-composer/src/lib/wired-elements/WiredElementsContainer.tsx
+++ flexbox-based-ui-composer/src/lib/wired-elements/WiredElementsContainer.tsx
@@ -1,54 +1,17 @@
-import { SelectionPanel } from '../ui-composer-sections/SelectionPanel';
-import { DraggableWithoutButtonLook } from "../DraggableWithoutButtonLook";
-import { useSelector } from 'react-redux';
-
+import { RenderSelectionPanel } from '../ui-composer-sections/RenderSelectionPanel';
 import { cardGroups } from './card-groups';
 
-function RenderSelectionPanel({
-  title,
-  entries,
-}: {
-  title: string;
-  entries: { id: string; card: JSX.Element }[];
-}) {
-  const displayStatus = useSelector(
-    (state: { composePlayground }) =>
-      state.composePlayground.selectionCardDisplayStatuses
-  );
-
-  function displayOn() {
-    let result = false;
-    cardGroups.forEach(({ title: cardGroupsTitle, cards }) => {
-      const cardTitles = cards.map(({ title }) => title);
-      if (cardTitles.includes(title)) {
-        result = !!displayStatus[cardGroupsTitle];
-      }
-    });
-    return result;
-  }
-
-  if (!displayOn()) {
-    return null;
-  }
-  return (
-    <SelectionPanel title={title}>
-      {entries.map((entry) => (
-        <div key={`${entry.id}-container`}>
-          <DraggableWithoutButtonLook
-            id={entry.id}
-          >
-            {entry.card}
-          </DraggableWithoutButtonLook>
-        </div>
-      ))}
-    </SelectionPanel>
-  );
-}
-
 export function WiredElementsContainer() {
-  return cardGroups.map(({ cards }) => {
-    return cards.map(({ title, entries }) => {
-      return <RenderSelectionPanel key={title} title={title} entries={entries} />;
+  return cardGroups.map(({ title: cardGroupTitle, cards }) => {
+    return cards.map(({ title: cardTitle, entries }) => {
+      return (
+        <RenderSelectionPanel
+          key={cardTitle}
+          cardTitle={cardTitle}
+          entries={entries}
+          cardGroupTitle={cardGroupTitle}
+        />
+      );
     });
   });
 }
