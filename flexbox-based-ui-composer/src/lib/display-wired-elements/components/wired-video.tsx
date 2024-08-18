37ad3eddc9cdbc2240e2a5e594f8febc45724792
Vishal Kumar <connectwithvkumar@gmail.com>
2024-08-18 16:43:07 -0400

    do not play an actual video on the video control

--- flexbox-based-ui-composer/src/lib/display-wired-elements/components/wired-video.tsx
+++ flexbox-based-ui-composer/src/lib/display-wired-elements/components/wired-video.tsx
@@ -1,3 +1,4 @@
 export function WiredVideo() {
-    return <wired-video autoplay muted loop src="http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4"></wired-video>
+    // return <wired-video autoplay muted loop src="http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4"></wired-video>
+    return <wired-video></wired-video>;
 }
