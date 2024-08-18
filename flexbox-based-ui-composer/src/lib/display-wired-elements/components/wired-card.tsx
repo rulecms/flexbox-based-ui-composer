7a04d4a6e04af2613c8e799ecf322802adfa09d5
Vishal Kumar <connectwithvkumar@gmail.com>
2024-08-18 15:16:28 -0400

    added a card without title as an option

--- flexbox-based-ui-composer/src/lib/display-wired-elements/components/wired-card.tsx
+++ flexbox-based-ui-composer/src/lib/display-wired-elements/components/wired-card.tsx
@@ -1,13 +1,20 @@
-export function WiredCard({elevation = "1", fill, title="Title", content = "Content"}: {
+export function WiredCard({
+  elevation = '1',
+  fill,
+  title = 'Title',
+  content = 'Content',
+  displayTitle = true,
+}: {
   elevation?: string;
   fill?: string;
   title?: string;
   content?: string;
+  displayTitle?: boolean;
 }) {
   return (
     <wired-card elevation={elevation} fill={fill}>
       <div>
-        <h2>{title || 'Title'}</h2>
+        {displayTitle && <h2>{title || 'Title'}</h2>}
         <p>{content || 'Content'}</p>
       </div>
     </wired-card>
