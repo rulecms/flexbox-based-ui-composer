491e4372e2bccd1abd1c4ef7206af527f365a1d2
Vishal Kumar <connectwithvkumar@gmail.com>
2024-08-23 09:32:10 -0400

    added some padding to wired divider

--- flexbox-based-ui-composer/src/lib/display-wired-elements/components/wired-divider.tsx
+++ flexbox-based-ui-composer/src/lib/display-wired-elements/components/wired-divider.tsx
@@ -1,3 +1,12 @@
 export function WiredDivider() {
-    return <wired-divider></wired-divider>;
+  return (
+    <div
+      style={{
+        marginTop: `var(--sl-spacing-small)`,
+        marginBottom: `var(--sl-spacing-small)`,
+      }}
+    >
+      <wired-divider></wired-divider>
+    </div>
+  );
 }
