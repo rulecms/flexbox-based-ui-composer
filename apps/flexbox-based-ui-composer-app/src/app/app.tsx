0545e72ec92f5b29d33b89cfb3688ca8382cc616
Vishal Kumar <connectwithvkumar@gmail.com>
2024-08-25 12:50:05 -0400

    favico and github link

--- apps/flexbox-based-ui-composer-app/src/app/app.tsx
+++ apps/flexbox-based-ui-composer-app/src/app/app.tsx
@@ -3,6 +3,7 @@ import { FlexboxBasedUiComposer } from '@rulecms/flexbox-based-ui-composer';
 import SlAvatar from '@shoelace-style/shoelace/dist/react/avatar/index.js';
 import SlRadioButton from '@shoelace-style/shoelace/dist/react/radio-button/index.js';
 import SlRadioGroup from '@shoelace-style/shoelace/dist/react/radio-group/index.js';
+import SlIcon from '@shoelace-style/shoelace/dist/react/icon/index.js';
 
 import '@shoelace-style/shoelace/dist/themes/light.css';
 import '@shoelace-style/shoelace/dist/themes/dark.css';
@@ -12,10 +13,9 @@ setBasePath(
   'https://cdn.jsdelivr.net/npm/@shoelace-style/shoelace@2.16.0/cdn/'
 );
 
-
 export function App() {
   const modifyTheme = (themeValue: string) => {
-    const htmlTagHandle = document.getElementsByTagName('html')[0]
+    const htmlTagHandle = document.getElementsByTagName('html')[0];
     if (themeValue === 'light') {
       htmlTagHandle.classList.remove('sl-theme-dark');
       htmlTagHandle.classList.add('sl-theme-light');
@@ -25,14 +25,14 @@ export function App() {
     }
   };
   return (
-      <><nav
+    <>
+      <nav
         style={{
           display: `flex`,
           justifyContent: `space-between`,
           alignItems: `center`,
           padding: `0 var(--sl-spacing-small)`,
         }}
-        
       >
         <div
           style={{
@@ -44,19 +44,34 @@ export function App() {
           <SlAvatar image="/assets/RuleCMS Logo.png" label="RuleCMS" />
           <h1>UI Mock Creator</h1>
         </div>
-        <SlRadioGroup
-          size="small"
-          label=""
-          name="theme-selector"
-          value='dark'
-          onSlChange={(event: any) => modifyTheme(event.target.value)}
+        <div
+          style={{
+            display: `flex`,
+            alignItems: `center`,
+            gap: `var(--sl-spacing-small)`,
+          }}
         >
-          <SlRadioButton value="light">Light</SlRadioButton>
-          <SlRadioButton value="dark">Dark</SlRadioButton>
-        </SlRadioGroup>
+          <SlRadioGroup
+            size="small"
+            label=""
+            name="theme-selector"
+            value="dark"
+            onSlChange={(event: any) => modifyTheme(event.target.value)}
+          >
+            <SlRadioButton value="light">Light</SlRadioButton>
+            <SlRadioButton value="dark">Dark</SlRadioButton>
+          </SlRadioGroup>
+          <a
+            href="https://github.com/rulecms/flexbox-based-ui-composer"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <SlIcon name="github" style={{ fontSize: `32px` }} />
+          </a>
+        </div>
       </nav>
       <FlexboxBasedUiComposer />
-      </>
+    </>
   );
 }
 
